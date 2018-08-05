9587be6f505652ed5164179c8df7993adefe54e2
billouboq <aurelien.boquet.web@gmail.com>
2018-08-05 22:57:05 +0200

    add normal example

--- examples/normal/index.js
+++ examples/normal/index.js
@@ -1,22 +1,26 @@
 "use strict";
 
-const slashCSS = require("../index");
+const glob = require("fast-glob");
 const postcss = require("postcss");
+const slashCSS = require("../../index");
 const fs = require("fs");
 
-const glob = require("fast-glob");
-
-glob("./**/*.css").then(files => {
-  console.log(files);
-});
+const inputFilePath = "../assets/normal.css";
+const targetFilePath = "../assets/*.critical.css";
 
 // plugin we will use in postcss
-// const pluginsUsed = [
-//   slashCSS({ targets: "./normal.critical.css" })
-// ]
+const pluginsUsed = [
+  slashCSS({ targets: targetFilePath })
+];
 
-// fs.readFile('./normal.css', async (err, css) => {
-//   const newCSS = postcss(pluginsUsed)
-//     .process(css, { from: 'src/app.css', to: 'dest/app.css' })
-//     fs.writeFile('dest/app.css', result.css, () => true)
-// })
+// this will read normal.css content, pass it to postcss
+// then this plugin will read all file with .critical.css at the end
+// check for duplicates between normal.css and those files
+// and return a new css content with those duplicates removed
+// aftre that we create a new file called result.normal.css with the new css content
+fs.readFile(inputFilePath, async (err, css) => {
+    const result = await postcss(pluginsUsed).process(css, {from: inputFilePath});
+    fs.writeFile('./result.normal.css', result.css, () => {
+        console.log("check result in the 'result.normal.css' file");
+    });
+})
